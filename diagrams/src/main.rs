55cd00b45d645f541d1bb56cbab5d04a88d61b73
Margo <margo.rozhkina@bk.ru>
2024-10-08 15:54:56 +0300

    -
    вывод данных по y

--- diagrams/src/main.rs
+++ diagrams/src/main.rs
@@ -1,32 +1,34 @@
 use std::collections::HashMap;
 
-const MAX: i32 = 200;
+const W: i32 = 100;
+const H: i32 = 20;
 
 fn main() {
-    let mut y: i32 = 0;
-    let mut step: i32 = 0;
     let mut x: HashMap<&str,i32> = HashMap::new();
 
-    input_data(&mut y, &mut step, &mut x);
-    create_diagram(y, step, x);
+    input_data(&mut x);
+    create_diagram(x);
 }
 
-fn input_data(y: &mut i32, step: &mut i32, x: &mut HashMap<&str,i32>) {
-    *y=-35;
-    *step=5;
+fn input_data(x: &mut HashMap<&str,i32>) {
     (*x).insert("зима",-23);
     (*x).insert("весна",10);
     (*x).insert("лето",27);
     (*x).insert("осень",17);
 }
 
-fn create_diagram(y: i32, step: i32, x: HashMap<&str,i32>) {
-    let max = x.values().max();
+fn create_diagram(x: HashMap<&str,i32>) {
+    let temp = x.clone();
+    let max = temp.values().max();
+    let temp = x.clone();
+    let min = temp.values().min();
     
-    if max!=None {
-        let matr: Vec<Vec<char>> = vec![vec!['-';MAX.try_into().unwrap()];((max.unwrap()-y)/step).try_into().unwrap()];
-        print_legenda(y, step, x);
-        print_diagram(matr);
+    if max!=None && min!=None {
+        let matr: Vec<Vec<char>> = vec![vec!['-';W.try_into().unwrap()];H.try_into().unwrap()];
+        let step: f64 = (max.unwrap()-min.unwrap()) as f64 / (H-1) as f64;
+
+        print_legenda(x);
+        print_diagram(matr, *max.unwrap() as f64,  *min.unwrap() as f64, step);
     }
     else {
         println!("Данные не найдены!");
@@ -34,28 +36,46 @@ fn create_diagram(y: i32, step: i32, x: HashMap<&str,i32>) {
 
 }
 
-fn print_diagram(matr: Vec<Vec<char>>) {
+fn print_diagram(matr: Vec<Vec<char>>, mut max: f64, min: f64, step: f64) {
+    let size: usize;
+
+    if format!("{:.2}",max).chars().count()>format!("{:.2}",min).chars().count() {
+        size = format!("{:.2}",max).chars().count();
+    }
+    else {
+        size = format!("{:.2}",min).chars().count();
+    }
+
     for row in &matr {
+        let mut size_max = format!("{:.2}",max).chars().count();
+        if size_max<size {
+            while size_max<size {
+                print!(" ");
+                size_max+=1;
+            }
+        }
+        print!("{:.2} |",max);
         for &value in row {
             print!("{}",value);
         }
+        max-=step;
         println!("");
     }
 }
 
-fn print_legenda(y: i32, step: i32, x: HashMap<&str,i32>) {
-    for _i in 0..MAX {
+fn print_legenda(x: HashMap<&str,i32>) {
+    for _i in 0..W {
         print!("━");
     }
     println!("");
-    print!("y={}    step={}", y, step);
+    print!("Данные: ");
     let mut count = 1;
     for (key,value) in &x {
         print!("    {}: ({} | {})",count,key,value);
         count+=1;
     }
     println!("");
-    for _i in 0..MAX {
+    for _i in 0..W {
         print!("━");
     }
     println!("");
--- diagrams/target/debug/deps/diagrams.d
+++ diagrams/target/debug/deps/diagrams.d
@@ -1,5 +1,5 @@
-C:\MyProjects\GitHub\Diagrams\diagrams\target\debug\deps\diagrams.exe: src/main.rs
+C:\Projects\Git\Diagrams\diagrams\target\debug\deps\diagrams.exe: src/main.rs
 
-C:\MyProjects\GitHub\Diagrams\diagrams\target\debug\deps\diagrams.d: src/main.rs
+C:\Projects\Git\Diagrams\diagrams\target\debug\deps\diagrams.d: src/main.rs
 
 src/main.rs:
