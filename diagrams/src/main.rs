82d92e05a86c16a1d437b9d22d410ca3acb5d7f7
Margo <margo.rozhkina@bk.ru>
2024-10-08 20:56:23 +0300

    -
    ввод данных (пока не работает)

--- diagrams/src/main.rs
+++ diagrams/src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
+use std::io;
 
 const W: i32 = 100;
 const H: i32 = 20;
+const MAX_DATA: i32 = (W-3)/4;
 
 fn main() {
     let mut x: HashMap<&str,f64> = HashMap::new();
@@ -10,11 +12,57 @@ fn main() {
     create_diagram(&mut x);
 }
 
-fn input_data(x: &mut HashMap<&str,f64>) {
-    (*x).insert("зима",-23.0);
-    (*x).insert("весна",10.0);
-    (*x).insert("лето",27.0);
-    (*x).insert("осень",17.0);
+fn input_data(x: &mut HashMap<&str,f64>) -> i32{
+    let mut f = 1;
+
+    println!("Введите данные в следующем формате: ключ значение\n
+              Для выхода введите q");
+
+    loop {
+        let mut data = String::new();
+        io::stdin().read_line(&mut data).expect("Ошибка");
+
+        let data = data.trim();
+
+        if data == "q" || f==0 {
+            break;
+        }
+
+        let temp: Vec<String> = data.split_whitespace().map(String::from).collect();
+
+        if temp.len()!=2 {
+            f=0;
+            println!("Неккоректные данные");
+        }
+        else {
+            let k: String = temp[0].clone();
+            let v: f64 = match temp[1].trim().parse::<f64>() {
+                Ok(val) => val,
+                Err(_) => {
+                    println!("Значение должно быть числом");
+                    f=0;
+                    continue;
+                }
+            };
+
+            x.insert(k.as_str(),v);
+        }
+
+    }
+
+    f
+    
+
+    // (*x).insert("зима",-23.0);
+    // (*x).insert("весна",10.0);
+    // (*x).insert("лето",27.0);
+    // (*x).insert("осень",17.0);
+
+    // (*x).insert("Аня",21.0);
+    // (*x).insert("Федор",6.0);
+    // (*x).insert("Кристина",43.0);
+    // (*x).insert("Петр",12.0);
+    // (*x).insert("Светлана",24.0);
 }
 
 fn create_diagram(x: &mut HashMap<&str,f64>) {
@@ -76,31 +124,44 @@ fn print_diagram(matr: &Vec<Vec<char>>, x: &mut HashMap<&str,f64>, mut max: f64,
 }
 
 fn print_legenda(x: &HashMap<&str,f64>, size: usize) {
+    let border = || {
+        for _i in 0..(W+size as i32) {
+            print!("━");
+        }
+        println!("");
+    };
+
+    let mes = |m: String| {
+        for _i in 0..((W-m.chars().count() as i32)/2) {
+            print!(" ");
+        }
+        print!("{}",m);
+        for _i in 0..((W-m.chars().count() as i32)/2) {
+            print!(" ");
+        }
+        println!("");
+    };
+    
+    border();
+
     let mut message = "Данные:".to_string();
     let mut count = 1;
     for (key,value) in x {
-        message.push_str(&format!("    {}: ({} | {})",count,key,value).to_string());
+        let temp = format!("    {}: ({} | {})",count,key,value).to_string();
+        if message.chars().count()+temp.chars().count() > W.try_into().unwrap() {
+            mes(message);
+            println!("");
+            message = temp;
+        }
+        else {
+            message.push_str(&temp);
+        }
         count+=1;
     }
     message.push_str("");
 
-    for _i in 0..(W+size as i32) {
-        print!("━");
-    }
-    println!("");
+    mes(message);
 
-    for _i in 0..((W-message.chars().count() as i32)/2) {
-        print!(" ");
-    }
-    print!("{}",message);
-    for _i in 0..((W-message.chars().count() as i32)/2) {
-        print!(" ");
-    }
-    println!("");
-
-    for _i in 0..(W+size as i32) {
-        print!("━");
-    }
-    println!("");
+    border();
     println!("");
 }
